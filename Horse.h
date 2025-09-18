bb17374f96fa6c1b28fb309c2b384b8c767045af
Firepeashooter1 <firepeashooter1@gmail.com>
2025-09-18 19:00:28 +0000

    More Horse Heads

--- Horse.h
+++ Horse.h
@@ -1,8 +1,17 @@
-#ifndef Horse_H
-#define Horse_H
+#ifndef Horse_H_EXIST
+#define Horse_H_EXIST
 
 class Horse {
 	private:
+		int position;
+		int id;
+		int trackLength;
 
 	public:
 		Horse();
+
+		void init(int id, int trackLength);
+		void advnace();
+		void printLane();
+		void isWinner();
+};//End class def
