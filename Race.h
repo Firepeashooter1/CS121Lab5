624141d279dc8c624afda67bbb0332098d836bc1
Firepeashooter1 <firepeashooter1@gmail.com>
2025-09-19 22:38:14 +0000

    Race

--- Race.h
+++ Race.h
@@ -1,2 +1,21 @@
 #ifndef Race_H_EXISTS
-#define Race_H_EXISTS
+#define Race_H_EXIST
+
+#include "Horse.h"
+
+class Race {
+private:
+    static const int NUM_HORSES = 5;
+    static const int TRACK_LENGTH = 10;
+    Horse horses[NUM_HORSES];
+
+public:
+    Race();
+    void run();
+    void display() const;
+    bool checkWinners() const;
+    void update();
+};
+
+#endif
+
