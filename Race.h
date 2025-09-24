88ef96c276dcd2f092e2219264e66a18908f22b7
Firepeashooter1 <firepeashooter1@gmail.com>
2025-09-24 17:21:07 +0000

    pesdo code for Race.h and acdenttally making a Horse.j file

--- Race.h
+++ Race.h
@@ -1,3 +1,23 @@
+/*ifndef and define Race_H
+ *
+ * include Horse.h
+ *
+ * class Race:
+ *  private:
+ *   static const int NUM_HORSES equals 5
+ *   sraric const int TRACK_LENGTH equals 10
+ *   Horse horse[NUM_HORSES]
+ *
+ *  public:
+ *   Race()
+ *   void run
+ *   void display const
+ *   void checkWinners const
+ *   void update
+ *
+ * endif
+ */
+
 #ifndef Race_H
 #define Race_H
 
