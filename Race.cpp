6ea2383c8f484d764ccebc2dfdcc4ca80188e6bd
Firepeashooter1 <firepeashooter1@gmail.com>
2025-09-24 17:15:02 +0000

    pesudo code to Race.cpp

--- Race.cpp
+++ Race.cpp
@@ -1,3 +1,43 @@
+/*include Race.h, iostream, ctime, and cstdlib
+ *
+ * Race Race:
+ *  srand(static_cast with an unsigned int with time(nullptr))
+ *
+ * void Race run:
+ *  while not checkWinner:
+ *   update()
+ *   display()
+ *   print Enter to contiune
+ *   cin.get to obtain enter key input
+ *
+ * print Race over Winner(s):
+ *
+ * for i (equals 0) less than NUM_HORSES and i is increase:
+ *  if horse[i] hasWon(TRACK_LENGTH):
+ *   print Horse (i+1)!
+ *
+ * void Race update:
+ *  for i (equal 0) less than NUM_HORSES and i is increase:
+ *   horse[i] advance
+ *
+ * bool Race checkWinners const:
+ *  for int (equal 0) less than NUM_HORSES and i is increase:
+ *   if horse[i] hasWon(TRACK_LENGTH):
+ *    return true else return false
+ *
+ * void Race display const:
+ *  print Current race status
+ *
+ *  for i (equal 0) less than NUM_HORSES and i is increase:
+ *   print Horse (i + 1);
+ *
+ *   for j (equal 0) less than horse[i].getPosition and j is increase:
+ *    print -
+ *
+ *   print >
+ *
+ */
+
 #include "Race.h"
 #include <iostream>
 #include <ctime>
