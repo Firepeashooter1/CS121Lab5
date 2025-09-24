512e0dbdc83d9aac4c22b671c2ad8c76eaebc695
Firepeashooter1 <firepeashooter1@gmail.com>
2025-09-24 16:40:49 +0000

    Added pesudo code to horse cpp

--- Horse.cpp
+++ Horse.cpp
@@ -1,3 +1,15 @@
+/* #include Horse.h and cstdlib 
+ * void Horse advance:
+ *  if rand % 2 equal 0:
+ *   increase the posisition
+ *
+ * int Horse getPosition const
+ *  return position
+ *
+ * bool Horse hasWom(int trackLength) const
+ *  return position greater than or equal to trackLength
+ */
+
 #include "Horse.h"
 #include <cstdlib>  // for rand()
 
